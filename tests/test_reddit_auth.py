347ede8bdf61357bb869a8a7b9afa17dba210ba8
cookie1986 <cookd.liv@gmail.com>
2026-04-24 15:35:06 +0100

    Add tests/

---
+++ tests/test_reddit_auth.py
@@ -0,0 +1,18 @@
+import praw
+from src.reddit_scraper import get_reddit
+
+def test_reddit_connection():
+    reddit = get_reddit()
+    # Basic type check
+    assert isinstance(reddit, praw.Reddit)
+    # Trigger API call
+    user = reddit.user.me()
+    assert user is None or isinstance(user, str)
+
+
+def test_can_read_subreddit():
+    reddit = get_reddit()
+    subreddit = reddit.subreddit("news")
+    post = next(subreddit.hot(limit=1))
+    # Confirm a non-empty string
+    assert post.title
