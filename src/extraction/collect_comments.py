20e4da0bf41a20ec62298d03df05939f02843c81
cookie1986 <cookd.liv@gmail.com>
2026-06-03 13:44:04 +0100

    Add sleep function to comment scraping

--- src/extraction/collect_comments.py
+++ src/extraction/collect_comments.py
@@ -1,10 +1,16 @@
+import time
+
 from src.extraction.client import get_reddit
 from config import NUM_COMMENTS
 
-def fetch_comments(post_id: str) -> list[dict]:
+def fetch_comments(
+        post_id: str,
+        sleep_seconds: float = 0.65
+        ) -> list[dict]:
     """Fetch the top comments for a given reddit post."""
     reddit = get_reddit()
     submission = reddit.submission(id=post_id)
+    time.sleep(sleep_seconds)
     submission.comment_sort = "top"
     submission.comments.replace_more(limit=0)
     top_comments = submission.comments[:NUM_COMMENTS]
